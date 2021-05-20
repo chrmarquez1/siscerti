e2bf79df1bda3e4983fadec61aa7e277206a2317
chrmarquez1 <chrmarquez1@gmail.com>
2021-05-20 11:40:53 +0000

    edit inicio.php

--- inicio.php
+++ inicio.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<h1>PRUEBA GITHUB</h1>
+<h1>PRUEBA GITHUB edit christian</h1>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
